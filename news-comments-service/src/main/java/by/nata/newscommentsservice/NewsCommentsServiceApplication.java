fb141e555de8f2438a65cf25718065e211c54d0f
NataPetra <nika.live.1@mail.ru>
2023-11-04 16:10:08 +0300

    fix MainClass

--- news-comments-service/src/main/java/by/nata/newscommentsservice/NewsCommentsServiceApplication.java
+++ news-comments-service/src/main/java/by/nata/newscommentsservice/NewsCommentsServiceApplication.java
@@ -2,16 +2,12 @@ package by.nata.newscommentsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@EntityScan(basePackages = "by.nata.newscommentsservice.database.model")
 @SpringBootApplication
 public class NewsCommentsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NewsCommentsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NewsCommentsServiceApplication.class, args);
+    }
 
 }
