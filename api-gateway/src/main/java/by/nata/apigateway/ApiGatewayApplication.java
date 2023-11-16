34b13751a69b855b1200b5c6c3afc44908d15930
NataPetra <nika.live.1@mail.ru>
2023-11-16 16:26:19 +0300

    add swagger ui for both services

--- api-gateway/src/main/java/by/nata/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/by/nata/apigateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package by.nata.apigateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,5 +12,4 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
--- news-comments-service/src/main/java/by/nata/newscommentsservice/controller/NewsController.java
+++ news-comments-service/src/main/java/by/nata/newscommentsservice/controller/NewsController.java
@@ -17,7 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,7 +36,6 @@ import java.util.List;
  * Logging:
  * - @MethodLog: Custom annotation for logging method input data.
  */
-//@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/app/news")
 @RequiredArgsConstructor
